3c631bfd89430f122be28657557473f6cc7db182
Jonatan Missora <jonatanjmissora@gmail.com>
2025-04-03 16:25:46 -0300

    UI fix, con los link y el themeswitcher, y logo

--- app/_components/Header/Header.tsx
+++ app/_components/Header/Header.tsx
@@ -4,13 +4,16 @@ import HeaderLink from './HeaderLink'
 import { NavLinks } from '@/app/_lib/constants'
 import MobilMenu from './MobilMenu'
 import DesktopThemeSwitcher from './DesktopThemeSwitcher'
+import LogoSVG from '@/app/_assets/LogoSVG'
 
 export default async function Header() {
 
   return (
     <header className="flex justify-between items-center px-6 sm:px-24 py-8 text-[var(--white)]">
 
-      <Link href={"/"} className="btn btn-ghost text-xl sm:text-xs 2xl:text-xl">K@to</Link>
+      <Link href={"/"} className="text-xl sm:text-xs 2xl:text-xl">
+        <LogoSVG className='size-7 text-[var(--color-primary)]' />
+      </Link>
 
       <nav className='sm:flex space-x-4 hidden'>
         {
@@ -18,9 +21,9 @@ export default async function Header() {
             <HeaderLink key={i} href={link.href} text={link.text} />
           ))
         }
+        <DesktopThemeSwitcher />
       </nav>
 
-      <DesktopThemeSwitcher />
       <MobilMenu />
 
     </header>
